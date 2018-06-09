79501613d417d37e93c0001a64db5cda2fbe6ad4
Adam Schmideg <adam@schmideg.net>
2018-06-09 13:22:34 +0200

    buggy bomberman

---
+++ src/hackerrank/bomberman.clj
@@ -0,0 +1,37 @@
+(ns hackerrank.bomberman)
+
+(defn replace-grid [replacement grid]
+  (map (fn [row]
+         (map replacement row))
+       grid))
+
+(defn explode-and-plant [grid]
+  (->>
+    (for [r (range (count grid))
+          c (range (count (first grid)))
+          :let [neighbors
+                (for [i [-1 0 1]
+                      j [-1 0 1]
+                      :when (or (zero? i) (zero? j))]
+                  (get-in grid [(+ r i) (+ c j)]))]]
+        (apply max (or (seq (remove nil? neighbors)) [0])))
+    (map #(- 1 %))
+    (partition (count (first grid)))))
+
+(defn stringify [grid]
+  (clojure.string/join "\n" (map #(apply str %) grid)))
+
+(defn numbered-bomberman [n grid]
+  (if (odd? n)
+    (->> grid
+         (iterate explode-and-plant)
+         (drop (dec (/ n 2)))
+         first)
+    (repeat (count grid) (repeat (count (first grid)) 1))))
+
+(defn bomberMan [n grid]
+    (->> grid
+      (replace-grid {\. 0, \0 1})
+      (numbered-bomberman n)
+      (replace-grid {0 \., 1 \0})
+      stringify))
---
+++ test/hackerrank/bomberman_test.clj
@@ -0,0 +1,29 @@
+(ns hackerrank.bomberman-test
+  (:require [clojure.test :refer :all]
+            [hackerrank.bomberman :refer :all]))
+
+(deftest replace-test
+  (is (= (replace-grid {0 \., 1 \0} [[0 1] [1 0]])
+         [[\. \0] [\0 \.]]))
+  (is (= (replace-grid {\. 0, \0 1} [".0" "0."])
+         [[0 1] [1 0]])))
+
+(deftest explode-test
+  (is (= (explode-and-plant [[0 0 1] [0 0 0]])
+         [[1 0 0] [1 1 0]])))
+
+(deftest stringify-test
+  (is (= (stringify [[\0 \0] [\. \.]])
+         "00\n..")))
+
+(deftest numbered-bomberman-test
+  (is (= (numbered-bomberman 2 [[9 9] [9 9]])
+         [[1 1] [1 1]]))
+  (is (= (numbered-bomberman 3 [[1 0] [0 1]])
+         [[0 0] [0 0]])))
+
+(deftest bomberman-test
+  (is (= (bomberMan 2 "0.0\n...")
+         "000\n000")))
+
+(run-tests)
