ff104cb53257685ac31be32c4fae4694c78020ab
Adam Schmideg <adam@schmideg.net>
2018-06-12 07:00:28 +0200

    rotate

--- src/hackerrank/larrys_array.clj
+++ src/hackerrank/larrys_array.clj
@@ -1,21 +1,24 @@
 (ns hackerrank.larrys-array)
 
 (defn rotate [v i]
-  (let [a (get v (dec i))
-        b (get v i)
-        c (get v (inc i))]
+  (let [a (get v i)
+        b (get v (dec i))
+        c (get v (- i 2))]
         ; [a b c] => [b c a]
        (-> v
-           (assoc (dec i) b)
-           (assoc i c)
-           (assoc (inc i) a))))
+           (assoc i b)
+           (assoc (dec i) c)
+           (assoc (- i 2) a))))
 
 (defn move-and-drop-greatest [v]
   (let [greatest (count v)
         i (.indexOf v greatest)]
-    (if (zero? i)
-      (subvec v 1)
-      (recur (rotate v i)))))
+    (cond
+      (zero? i) (subvec v 1)
+      (= i 1) (recur (-> v
+                         (rotate 2)
+                         (rotate 2)))
+      :else (recur (rotate v i)))))
 
 (defn larrysArray [A]
   (loop [v (vector (reverse A))]
--- test/hackerrank/larrys_array_test.clj
+++ test/hackerrank/larrys_array_test.clj
@@ -4,8 +4,11 @@
 
 (deftest all
   (testing "rotate"
-    (is (= (rotate [2 3 4] 1) [3 4 2]))
-    (is (= (rotate [2 3 4 5] 1) [3 4 2 5]))
-    (is (= (rotate [2 3 4 5] 3) [2 4 5 3]))))
+    (is (= (rotate [2 3 4] 2) [4 2 3]))
+    (is (= (rotate [2 3 4 5] 2) [4 2 3 5])))
+
+  (testing "move-and-drop-greatest"
+    (is (= (move-and-drop-greatest [4 1 2 3]) [1 2 3]))
+    (is (= (move-and-drop-greatest [1 3 2 4]) [3 1 2]))))
 
 (run-tests)
