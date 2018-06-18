7fbdcaf51dd687fef2b9d7d110ff6e6a25603eb1
Adam Schmideg <adam@schmideg.net>
2018-06-18 22:51:31 +0200

    better but not fast enough

--- src/hackerrank/fraudulent_activity.clj
+++ src/hackerrank/fraudulent_activity.clj
@@ -1,17 +1,45 @@
 (ns hackerrank.fraudulent-activity)
 
-(defn median [v]
-  (let [sorted (vec (sort v))
-        c (count sorted)
+(defn insertion-point [i]
+  (if (neg? i) (* -1 (inc i)) i))
+
+(defn insert-into-sorted [xs x]
+  (let [loc (insertion-point (java.util.Collections/binarySearch xs x))
+        before (subvec xs 0 loc)
+        after (subvec xs loc)]
+    (-> before
+      (into [x])
+      (into after))))
+
+(defn remove-from-sorted [xs x]
+  (let [loc (insertion-point (java.util.Collections/binarySearch xs x))
+        before (subvec xs 0 loc)
+        after (subvec xs (inc loc))]
+    (into before after)))
+
+(defn median-of-sorted [sorted]
+  (let [c (count sorted)
         half (quot c 2)]
     (if (odd? c)
       (get sorted half)
       (/ (+ (get sorted half) (get sorted (dec half))) 2))))
 
+(defn median [v]
+  (median-of-sorted (vec (sort v))))
+
+(defn remove-insert [sorted [remove insert]]
+  (-> (vec sorted)
+      (remove-from-sorted remove)
+      (insert-into-sorted insert)))
+
 ; Complete the activityNotifications function below.
 (defn activityNotifications [expenditure d]
   (let [trailing (partition d 1 expenditure)
         check (drop d expenditure)
-        warnings (map (fn [t c] (<= (* 2 (median t)) c)) trailing check)]
+        sorted (vec (sort (take d expenditure)))
+        new (map #(vector %1 %2) expenditure (drop d expenditure))
+        sorted-chunks (reductions remove-insert sorted new)
+        medians (map median-of-sorted sorted-chunks)
+        warnings (map (fn [m c] (<= (* 2 m) c)) medians check)]
     (count (filter identity warnings))))
 
--- test/hackerrank/fraudulent_activity_test.clj
+++ test/hackerrank/fraudulent_activity_test.clj
@@ -2,8 +2,20 @@
   (:require [clojure.test :refer :all]
             [hackerrank.fraudulent-activity :refer :all]))
 
+(deftest insert-test
+  (is (= (insert-into-sorted [2 4 6] 5) [2 4 5 6]))
+  (is (= (insert-into-sorted [2 4 6] 4) [2 4 4 6]))
+  (is (= (insert-into-sorted [2 4 6] 1) [1 2 4 6]))
+  (is (= (insert-into-sorted [2 4 6] 8) [2 4 6 8])))
+
+(deftest remove-test
+  (is (= (remove-from-sorted [2 4 6] 4) [2 6]))
+  (is (= (remove-from-sorted [2 4 4 6] 4) [2 4 6]))
+  (is (= (remove-from-sorted [2 4 6] 2) [4 6]))
+  (is (= (remove-from-sorted [2 4 6] 6) [2 4])))
+
 (deftest simple-test
-  (is (= (activityNotifications [2 3 4 2 3 6 8 4 5] 5)
-        2)))
+    (is (= (activityNotifications [2 3 4 2 3 6 8 4 5] 5)
+          2)))
 
 (run-tests)
